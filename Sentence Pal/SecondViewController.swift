6397c386c8c350396a90c34270396b477a38dbc9
Jon Simington <jon.simington@mst.edu>
2015-12-01 13:55:45 -0600

    Change name

--- Sentence Pal/SecondViewController.swift
+++ Sentence Pal/SecondViewController.swift
@@ -3,7 +3,7 @@
 //  Sentence Pal
 //
 //  Created by Jon Simington on 9/27/15.
-//  Copyright (c) 2015 JaySik Studios. All rights reserved.
+//  Copyright (c) 2015 Jon Simington. All rights reserved.
 //
 
 import UIKit
